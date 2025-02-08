099406375d5c936051d0097ac9549000e0b89993
Udara <usudarasubodhitha@gmail.com>
2025-02-08 15:05:06 +0530

    Connect the cloudinary , user and product schemas

---
+++ backend/models/productModel.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image:{
+        type: Array,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    }, 
+    subCategory: {
+        type: String,
+        required: true
+    },
+    sizes:{
+        type: Array,
+        required: true
+    },
+    bestseller:{
+        type: Boolean,
+        
+    },
+    date: {
+        type: Number,
+        required: true
+    }
+
+
+}) 
+
+const productMOdel = mongoose.model.prduct || mongoose.model('product', productSchema);
+
+export default productMOdel;
