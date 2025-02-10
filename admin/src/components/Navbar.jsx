76ff73d46fd24d9acc470c59e8875441382cbe7f
Udara <usudarasubodhitha@gmail.com>
2025-02-10 08:18:53 +0530

    Admin UI created

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className="flex items-center py-2 px-4 justify-between">
+      <img className="w-40 h-20 max-w-[80px] max-h-[80px]" src={assets.logo} alt="logo" />
+      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
